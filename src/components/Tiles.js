dad59fc81193f2330c3e204609b6e3707a32c7b2
Aoi Ueki <129098557+Aoi-U@users.noreply.github.com>
2023-12-30 22:46:51 -0700

    tile content made into 1 array. bottom border for tile elements

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -4,26 +4,75 @@ export default function Tiles(props) {
   const [selectedTile, setSelectedTile] = useState([
     {
       title: "Education",
-      subTitle: ["University of Calgary", "Calgary Hoshuko Japanese School"],
-      desc: "Majoring in Computer Science. Going into my 3rd year of university next fall!",
+
+      content: [
+        {
+          sub: "University of Calgary",
+          desc: "Majoring in Computer Science. Going into my 3rd year of University next fall!",
+        },
+        {
+          sub: "Calgary Hoshuko Japanese School",
+          desc: "Attended from grade 1 to 6. Native and fluent in speaking Japanese",
+        },
+      ],
       active: true,
     },
     {
       title: "Experience",
-      subTitle: ["Chicken On The Way", "Calgary Hoshuko Japanese School"],
-      desc: "Attended Calgary Hoshuko from grade 1 to 6. Native and fluent in speaking Japanese",
+      content: [
+        {
+          sub: "Chicken On The Way",
+          desc: "Worked for 4 years as a cashier and cook at Chicken On The Way",
+        },
+        {
+          sub: "Calgary Hoshuko Japanese School",
+          desc: "Volunteered at Calgary Hoshuko's annual sports festival as a demonstrator",
+        },
+        {
+          sub: "Summer Camp",
+          desc: "Volunteered looking after children and helping with activities",
+        },
+        {
+          sub: "News Paper Delivery",
+          desc: "Bundled and delivered newspapers around my neighborhood for 2 years",
+        },
+        {
+          sub: "Vending machine software",
+          desc: "Designed and implemented a full scale vending machine software for a university project",
+        },
+      ],
       active: false,
     },
     {
       title: "Skills",
-      subTitle: ["Programming Languages"],
-      desc: "C, C++, Java, Python, JavaScript, HTML, CSS",
+      content: [
+        {
+          sub: "Programming languages",
+          desc: "C, C++, Java, Python, JavaScript, HTML, CSS, React",
+        },
+        {
+          sub: "Thinking",
+          desc: "Calm and collected, able to think clearly and logically under pressure",
+        },
+        {
+          sub: "Teamwork",
+          desc: "Able to work well with others and communicate effectively",
+        },
+      ],
       active: false,
     },
     {
       title: "Hobbies",
-      subTitle: ["Soccer", "Video Games", "Anime"],
-      desc: "I play soccer in my free time. I also enjoy playing video games and watching anime.",
+      content: [
+        {
+          sub: "Video Games",
+          desc: "Mainly play League of Legends and Apex Legends but also many more!",
+        },
+        {
+          sub: "Anime",
+          desc: "Watched hundreds of anime series and read many manga series as well!",
+        },
+      ],
       active: false,
     },
   ]);
@@ -36,10 +85,10 @@ export default function Tiles(props) {
     flexGrow: "0",
   };
 
-  const changeTile = (e) => {
+  const changeTile = (activeTile) => {
     setSelectedTile(
       selectedTile.map((tile, index) => {
-        if ("tile" + index == e.target.id) {
+        if (index == activeTile) {
           tile.active = true;
         } else {
           tile.active = false;
@@ -57,9 +106,15 @@ export default function Tiles(props) {
           className="tile"
           id={"tile" + index}
           key={index}
-          onClick={changeTile}
+          onClick={() => changeTile(index)}
         >
           <h2>{tile.title}</h2>
+          {tile.content.map((content) => (
+            <div className="tile-content">
+              <h3>{content.sub}</h3>
+              <p>{content.desc}</p>
+            </div>
+          ))}
         </span>
       ))}
     </div>
